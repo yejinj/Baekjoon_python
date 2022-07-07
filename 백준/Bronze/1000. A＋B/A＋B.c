d42d6a7bccdcb595cbf032fd7d059eb4bfdc93fd
yejinj <80540445+yejinj@users.noreply.github.com>
2022-07-07 22:54:29 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int a, b;
+	
+	scanf("%d %d", &a, &b);
+	printf("%d\n", a + b);
+	
+	return 0;
+}
